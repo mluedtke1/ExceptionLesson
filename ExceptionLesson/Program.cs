1d70854dce33eb0b2dcd862ba5beda705c457635
Matthew Luedtke <MLuedtke@tql.com>
2021-05-24 12:30:15 -0400

    Add project files.

---
+++ ExceptionLesson/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ExceptionLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Level1();
+        }
+        static void Level1()
+        {
+            Level2();
+        }
+        static void Level2()
+        {
+            Level3();
+        }
+        static void Level3()
+        {
+            var n = 1;
+            var d = 0;
+            try
+            {
+                var e = n / d;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"Exception is {ex.Message}");
+            }
+        }
+
+
+    }
+}
